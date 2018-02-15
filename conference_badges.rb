eddd24f28298acfda21a57cc544883e6823984ae
James Lotruglio <jimlotruglio@yahoo.com>
2018-02-15 01:02:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,8 @@ def badge_maker(name)
   end
 
 def batch_badge_creator(array)
-array.collect do |name|
-  puts "Hello, my name is #{name}."
+new_array= []
+array.each do |name|
+  new_array << puts "Hello, my name is #{name}."
   end
 end
